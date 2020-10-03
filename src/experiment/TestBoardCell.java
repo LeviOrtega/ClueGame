cc9cb48ab7c920a24221c87729fbb7bbee02cfae
LeviOrtega <leviortega@mymail.mines.edu>
2020-10-03 15:51:57 -0600

    Finalized skeleton for TestBoardCell

--- src/experiment/TestBoardCell.java
+++ src/experiment/TestBoardCell.java
@@ -1,5 +1,35 @@
 package experiment;
 
-public class TestBoardCell {
+import java.util.Set;
+import java.util.TreeSet;
 
+public class TestBoardCell {
+	
+	private int row;
+	private int column;
+	private Set<TestBoardCell> adjList;
+	
+	private TestBoardCell(int row, int column) {
+		this.row = row;
+		this.column = column;
+		this.adjList = new TreeSet<TestBoardCell>();
+	}
+	
+	public void setRoom(boolean partOfRoom) {
+		
+	}
+	
+	public boolean isRoom() {
+		return false;
+	}
+	
+	
+	public void setOccupied(boolean occupied) {
+		
+	}
+	
+	public boolean isOccupied() {
+		return false;
+	}
+	
 }
