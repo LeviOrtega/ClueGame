79375e7d801ec77b6d5b2b43265b518dff5908ad
Anthony <adesiato@mymail.mines.edu>
2020-10-03 16:10:04 -0600

    Added methods within TestBoard class.

--- src/experiment/TestBoard.java
+++ src/experiment/TestBoard.java
@@ -1,5 +1,34 @@
 package experiment;
 
-public class TestBoard {
+import java.util.Set;
 
+public class TestBoard {
+	private int rowNum;
+	private int columnNum;
+	private TestBoardCell[][] board;
+	private Set<TestBoardCell> targets;
+	
+	public TestBoard(int rowNum, int columnNum) {
+		this.rowNum = rowNum;
+		this.columnNum = columnNum;
+		
+		board = new TestBoardCell[rowNum][columnNum];
+		
+	}
+	
+	public TestBoardCell getCell(int row, int col) {
+		return board[row][col];
+	}
+	
+	public void calcTargets(TestBoardCell startCell, int pathLength) {
+		
+		
+	}
+	
+	public Set<TestBoardCell> getTargets(){
+		return targets;
+	}
+	
+	
+	
 }
