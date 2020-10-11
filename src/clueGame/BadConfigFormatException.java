69d7f3dc4249a91e70bdbf323a1bc3133cefb201
LeviOrtega <leviortega@mymail.mines.edu>
2020-10-11 16:45:55 -0600

    Only one exception tests fail

--- src/clueGame/BadConfigFormatException.java
+++ src/clueGame/BadConfigFormatException.java
@@ -1,10 +1,21 @@
 package clueGame;
 
 
-public class BadConfigFormatException extends Exception {
+public class BadConfigFormatException extends RuntimeException {
 
+	private String message;
+	
 	public BadConfigFormatException() {
 		super("File not formatted correctly");
+	}
+	
+	public BadConfigFormatException(String message) {
+		this.message = message;
+	}
+	
+	
+	public String getMessage() {
+		return this.message;
 	}
 	
 	
--- src/clueGame/Board.java
+++ src/clueGame/Board.java
@@ -14,6 +14,8 @@ public class Board {
 	private static Board theInstance = new Board();
 	private int numRows;
 	private int numColumns;
+	public final String ROOM = "Room";
+	public final String SPACE = "Space";
 	private String layoutConfigFile;
 	private String setupConfigFile;
 	private BoardCell[][] board;
@@ -21,7 +23,7 @@ public class Board {
 	private Map<Character, Room> roomMap;
 	private Set<BoardCell> visited;
 	private Set<BoardCell> targets;
-	
+
 	private Board() {}
 
 	public static Board getInstance() {
@@ -35,21 +37,18 @@ public class Board {
 
 	public Room getRoom(BoardCell cell) {
 		return roomMap.get(cell.getInitial());
-		
+
 	}
-	
+
 	public Room getRoom(Character c) {
-		
+
 		return roomMap.get(c);
 	}
-	
-	public void initialize() {
+
+	public void initialize() throws BadConfigFormatException{
 		roomMap = new HashMap<Character, Room>();
-		loadLayoutConfig();
 		loadSetupConfig();
-		
-		
-		
+		loadLayoutConfig();
 		this.targets = new HashSet<BoardCell>();
 		this.visited = new HashSet<BoardCell>();
 		for (int i = 0; i < board.length; i++) {
@@ -58,69 +57,72 @@ public class Board {
 				generateBoardCellType(board[i][j]);
 			}
 		}
-		
+
 		generateBoardAdjList();
 	}
-	
+
 	public void generateBoardCellType(BoardCell bc) {
 		int row = bc.getRow();
 		int col = bc.getColumn();
 		String bcString = boardString[row][col];
 		if (bcString.length() > 1) {			// we only want to evaluate strings with two or more chars, they are the doors, centers, etc.
 			Character lastChar = bcString.charAt(bcString.length() -1);
-		
+
 			switch (lastChar) {
-				case 'v': {
-					bc.setDoorDirection(DoorDirection.DOWN);
-					bc.setDoorway(true);
-					break;
-				}
-				case '>': {
-					bc.setDoorDirection(DoorDirection.RIGHT);
-					bc.setDoorway(true);
-					break;
-				}
-				case '<': {
-					bc.setDoorDirection(DoorDirection.LEFT);
-					bc.setDoorway(true);
-					break;
-				}
-				case '^': {
-					bc.setDoorDirection(DoorDirection.UP);
-					bc.setDoorway(true);
-					break;
-				}
-				case '*':{
-					roomMap.get(bc.getInitial()).setCenterCell(bc);
-					bc.setRoomCenter(true);
-					bc.setRoom(true);
-					break;
-				}
-				case '#': {
-					roomMap.get(bc.getInitial()).setLabelCell(bc);
-					bc.setRoomLabel(true);
-					bc.setRoom(true);
-					break;
-				}
-				default: {
-					bc.setSecretPassage(lastChar);
-					break;
-				}
+			case 'v': {
+				bc.setDoorDirection(DoorDirection.DOWN);
+				bc.setDoorway(true);
+				break;
+			}
+			case '>': {
+				bc.setDoorDirection(DoorDirection.RIGHT);
+				bc.setDoorway(true);
+				break;
+			}
+			case '<': {
+				bc.setDoorDirection(DoorDirection.LEFT);
+				bc.setDoorway(true);
+				break;
+			}
+			case '^': {
+				bc.setDoorDirection(DoorDirection.UP);
+				bc.setDoorway(true);
+				break;
+			}
+			case '*':{
+				roomMap.get(bc.getInitial()).setCenterCell(bc);
+				bc.setRoomCenter(true);
+				bc.setRoom(true);
+				break;
+			}
+			case '#': {
+				roomMap.get(bc.getInitial()).setLabelCell(bc);
+				bc.setRoomLabel(true);
+				bc.setRoom(true);
+				break;
+			}
+			default: {
+				bc.setSecretPassage(lastChar);
+				break;
+			}
 			}
 		}
-	
+
 	}
-	
-	public void loadSetupConfig() {
+
+	public void loadSetupConfig() throws BadConfigFormatException{			// txt file loader
 		try {
 			File setup = new File("data/" + setupConfigFile);
 			Scanner sc = new Scanner(setup);
-		
+
 			while (sc.hasNextLine()) {
 				String line = sc.nextLine();
-				
+
 				if (!(line.split(" ")[0].equals("//"))) {
 					String[] roomInfo = line.split(", ");
+					if (!(roomInfo[0].equals(SPACE)) && !(roomInfo[0].equals(ROOM))) {
+						throw new BadConfigFormatException(setupConfigFile + " does not have only " + SPACE + " or " + ROOM + " card types.");
+					}
 					Room room = new Room(roomInfo[1]);
 					roomMap.put(roomInfo[2].charAt(0), room);
 					/*
@@ -135,16 +137,14 @@ public class Board {
 		catch(FileNotFoundException e) {
 			System.out.println("File, " + setupConfigFile + " could not be opened.");
 		}
-		
-		
-		
-		
+
 	}
 
-	public void loadLayoutConfig() {
+	public void loadLayoutConfig() throws BadConfigFormatException {		// CSV file loader
 		int colLen = 0;
 		int rowLen = 0;
-		
+
+
 		try {
 			File layout = new File("data/" + layoutConfigFile);
 			Scanner sc = new Scanner(layout);
@@ -154,25 +154,38 @@ public class Board {
 			rowLen = 1;		//because we used the sc.nextLine() before, we are now down one row
 			while (sc.hasNextLine()) {
 				rowLen ++;
-				sc.nextLine();
+				String[] colCheck = sc.nextLine().split(",");		// calls sc.nextLine() and gives a string of len of col for checking to see if all cols are same lenght
+				if (colCheck.length != colLen) {
+					throw new BadConfigFormatException(layoutConfigFile + " does not have correctly formated columns.");
+				}
 			}
-			
+
 			sc.close();
 			board = new BoardCell[rowLen][colLen];
 			boardString = new String[rowLen][colLen];
-			
-			int i = 0;
+
+			int b = 0;
 			sc = new Scanner(layout);
 			while (sc.hasNextLine()) {
-				boardString[i] = sc.nextLine().split(",");		// .split returns our row of strings for boardString
-				i++;
+				boardString[b] = sc.nextLine().split(",");		// .split returns our row of strings for boardString
+				b++;
 			}
 			sc.close();
 		}
 		catch(FileNotFoundException e) {
 			System.out.println("File " + layoutConfigFile + " cannot be opened.");
 		}
-		
+
+		for (int i = 0; i < boardString.length; i++) {
+			for (int j = 0; j < boardString[0].length; j++) {
+				System.out.println(roomMap.get(boardString[i][j].charAt(0)));
+				if (roomMap.get(boardString[i][j].charAt(0)) == null) {
+					System.out.println("hello");
+					throw new BadConfigFormatException(layoutConfigFile + " has incorrect rooms");
+				}
+			}
+		}
+
 		this.numColumns = colLen;
 		this.numRows = rowLen;
 	}
@@ -186,12 +199,12 @@ public class Board {
 			}
 		}*/
 	}
-	
+
 	public BoardCell getCell(int row, int col) {
 		return board[row][col];
 		//return board[row][col];
 	}
-	
+
 	public void calcAdjList(BoardCell bc) {
 		// Up, down, left, right
 		/*if (bc.getRow() - 1 >= 0) { 
@@ -207,10 +220,10 @@ public class Board {
 			bc.addToAdjList(getCell(bc.getRow(), bc.getColumn() + 1));
 		}*/
 	}
-	
+
 	public void calcTargets(BoardCell startCell, int pathLength) {
 		/*visited.add(startCell);
-		
+
 		for (BoardCell tbc: startCell.getAdjList()) {
 			if (!(visited.contains(tbc))) {
 				visited.add(tbc);
@@ -230,7 +243,7 @@ public class Board {
 	public void clearTargets() {
 		//targets.clear();
 	}
-	
+
 	public Set<BoardCell> getTargets(){
 		return targets;
 	}
@@ -242,5 +255,5 @@ public class Board {
 	public int getNumRows() {
 		return this.numRows;
 	}
-	
+
 }
